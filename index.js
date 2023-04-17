bc29ef66ea8b78c0997eb02e7327480d4d862766
Puja Agarwal <pujuagarwal5263@gmail.com>
2023-04-17 11:52:05 +0530

    completed new model

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { Configuration, OpenAIApi } = require("openai");
 const express = require("express");
+const cors = require('cors');
 require('dotenv').config()
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const configuration = new Configuration({
